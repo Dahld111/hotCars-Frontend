cd38c0778918ba106e118c1aa94679d9b21ff257
Mateo <silvamateo123@gmail.com>
2025-08-28 15:32:07 -0500

    Se agregegan nuevas pantallas, ciudades y de modifica la conexion a atlas

---
+++ src/app/pages/user-reser/user-reser.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-reser',
+  imports: [],
+  templateUrl: './user-reser.html',
+  styleUrl: './user-reser.css'
+})
+export class UserReser {
+
+}
